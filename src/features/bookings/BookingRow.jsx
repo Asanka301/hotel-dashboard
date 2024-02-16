9c9a54b46f44c64f18b00c01a9a050e9aa71922a
Asanka301 <asankagamage301@gmail.com>
2024-02-16 14:34:33 +0530

    made changes

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -49,11 +49,11 @@ const Amount = styled.div`
 function BookingRow({
   booking: {
     id: bookingId,
-    created_at,
+
     startDate,
     endDate,
     numNights,
-    numGuests,
+
     totalPrice,
     status,
     guests: { fullName: guestName, email },
