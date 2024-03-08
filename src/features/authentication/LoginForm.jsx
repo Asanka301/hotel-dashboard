1cf79d4f1cd4ab3e41db4628c3206da9c706bb07
Asanka301 <asankagamage301@gmail.com>
2024-03-08 18:27:11 +0530

    edit login

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("asanka@gmail.com");
+  const [password, setPassword] = useState("1234");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
